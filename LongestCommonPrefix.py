116f3bf7fdc00a996d63aa55af66a3acad403274
Pratiksha-Nalavade <pratikshanalavade26@gmail.com>
2026-06-04 22:40:05 +0530

    Create LongestCommonPrefix.py

---
+++ LongestCommonPrefix.py
@@ -0,0 +1,16 @@
+# LeetCode Problem: Longest Common Prefix
+# Difficulty: Easy 
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+        prefix=strs[0]
+
+        for s in strs[1:]:
+            while s[:len(prefix)] != prefix:
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+
+        return prefix
+        
